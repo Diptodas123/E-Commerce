103d019ac5b6ae2813648983c7b8beee258f609f
diptodas <dasdipto2004@gmail.com>
2024-02-21 21:43:59 +0530

    Update PageNavigation in SingleProduct page

--- src/Components/PageNavigation.jsx
+++ src/Components/PageNavigation.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const PageNavigation = ({ title }) => {
     return (
         <Wrapper>
-            <NavLink to={"/"}>Home</NavLink>&nbsp;/&nbsp;<NavLink to={"/products"}>products</NavLink>&nbsp;/&nbsp;{title}
+            <NavLink to={"/"}> Home</NavLink>&nbsp;/&nbsp;<NavLink to={"/products"}>products</NavLink>&nbsp;/&nbsp;{title}
         </Wrapper>
     )
 }
