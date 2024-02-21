ca19816901bac55a0f9e49df6d4b895735b8ef87
diptodas <dasdipto2004@gmail.com>
2024-02-21 21:57:50 +0530

    Update Products page

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -7,7 +7,13 @@ const ProductList = () => {
   const { filterProducts, gridView } = useFilterContext();
 
   if (filterProducts.length === 0) {
-    return <div style={{ padding: "9rem 0", maxWidth: "120rem", textAlign: "center" }} > No Data </div>
+    return (
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <img src="https://img.freepik.com/free-vector/no-data-concept-illustration_114360-2506.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1708473600&semt=ais"
+          style={{ padding: "9rem 0", maxWidth: "120rem" }}
+          alt="" />
+      </div>
+    )
   }
 
   if (gridView) {
